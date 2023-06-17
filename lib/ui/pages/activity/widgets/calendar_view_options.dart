ccd775aacf4ded7eab7f87f71d89c3d653519d65
tuancoi19 <quangtuandam19@gmail.com>
2023-06-18 04:15:07 +0700

    feat: add new locale texts

--- lib/ui/pages/activity/widgets/calendar_view_options.dart
+++ lib/ui/pages/activity/widgets/calendar_view_options.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:tasky/common/app_colors.dart';
 import 'package:tasky/common/app_text_styles.dart';
+import 'package:tasky/generated/l10n.dart';
 import 'package:tasky/ui/widgets/buttons/app_button.dart';
 
 class CalendarViewOptions extends StatelessWidget {
@@ -30,7 +31,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.day);
               },
-              title: 'Day View',
+              title: S.current.day_view,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -41,7 +42,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.week);
               },
-              title: 'Week View',
+              title: S.current.week_view,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -52,7 +53,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.month);
               },
-              title: 'Month View',
+              title: S.current.month_view,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -63,7 +64,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.schedule);
               },
-              title: 'Schedule',
+              title: S.current.schedule,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -74,7 +75,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.workWeek);
               },
-              title: 'Work Week',
+              title: S.current.work_week,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -85,7 +86,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.timelineDay);
               },
-              title: 'Timeline Day',
+              title: S.current.timeline_day,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -96,7 +97,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.timelineWeek);
               },
-              title: 'Timeline Week',
+              title: S.current.timeline_week,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -107,7 +108,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.timelineWorkWeek);
               },
-              title: 'Timeline Work Week',
+              title: S.current.timeline_work_week,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
@@ -118,7 +119,7 @@ class CalendarViewOptions extends StatelessWidget {
                 Get.back();
                 onChange.call(CalendarView.timelineMonth);
               },
-              title: 'Timeline Month',
+              title: S.current.timeline_month,
               textStyle: AppTextStyle.blackS18Bold,
               backgroundColor: AppColors.buttonBGWhite,
               cornerRadius: 8.r,
